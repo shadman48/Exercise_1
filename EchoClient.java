805b6bfe95ed0b1eff104664abc1b698085c08f1
Peter <shadman48@gmail.com>
2017-09-28 12:19:37 -0700

    EchoServer and EchoClient working.

--- EchoClient.java
+++ EchoClient.java
@@ -2,28 +2,47 @@
 import java.io.InputStream;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public final class EchoClient {
 
     public static void main(String[] args) throws Exception {
-        try (Socket socket = new Socket("localhost", 22222)) {
+        try (Socket socket = new Socket("localhost", 22222)) 
+        {
             InputStream is = socket.getInputStream();
             InputStreamReader isr = new InputStreamReader(is, "UTF-8");
             BufferedReader br = new BufferedReader(isr);
             System.out.println(br.readLine());
+       
+            PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
+	        
+	        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+	        BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+	        String input = "";
+	        
+	        System.out.print("Client > ");
+        	
+	        while((input = stdIn.readLine()) != null)
+	        {
+	        	out.println(input);
+	        	if(!input.equalsIgnoreCase("exit"))
+	        	{
+	        		System.out.println("Server > " + in.readLine());
+	        	}
+	        	
+	        	System.out.print("Client > ");
+	        	 
+	        }
+	        
+	        System.out.print("GoodBye.");
+        	
         }
     }
 }
-
-
-
-
-
-
-
-
-
 
 
 
--- EchoServer.java
+++ EchoServer.java
@@ -1,6 +1,10 @@
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -8,15 +12,36 @@ public final class EchoServer {
 
     public static void main(String[] args) throws Exception {
         try (ServerSocket serverSocket = new ServerSocket(22222)) {
-            while (true) {
-                try (Socket socket = serverSocket.accept()) {
+            		
+        	System.out.println("Server is now online.");
+    		
+        	while (true) 
+        	{
+                try (Socket socket = serverSocket.accept()) 
+                {
                     String address = socket.getInetAddress().getHostAddress();
                     System.out.printf("Client connected: %s%n", address);
                     OutputStream os = socket.getOutputStream();
                     PrintStream out = new PrintStream(os, true, "UTF-8");
                     out.printf("Hi %s, thanks for connecting!%n", address);
+                    
+                
+                    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                  
+                    String s;
+                    while((s = in.readLine()) != null)
+                    {
+                    	if(s.equalsIgnoreCase("exit"))
+                    		System.out.printf("Client disconnected: %s%n", address);
+                    	out.println(s);
+                    }
+                    
+                    
                 }
+                
             }
         }
     }
 }
+
+
